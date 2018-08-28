c6a6b2f98b21a47b7f8b6f3fd799d66c655de983
freddie <freddie.yang@139.com>
2018-08-28 23:19:21 +0800

    fix UART enable fail

--- src/main.c
+++ src/main.c
@@ -65,17 +65,32 @@ static uint32_t dev_uuid[4];
 static void system_clock_config(void);
 static void system_info_output(void);
 
-static void led_init(void);
-static void led_on(void);
-static void led_off(void);
-static void led_toggle(void);
-static bool led_state(void);
-
 static void ble_app_on_sync(void);
-
 static void ble_host_thread(void * arg);
 
-/* Private function prototypes -----------------------------------------------*/
+static void led_init(void);
+
+static inline void led_on(void)
+{
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_RESET);
+}
+
+static inline void led_off(void)
+{
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_SET);
+}
+
+static inline void led_toggle(void)
+{
+  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_2);
+}
+
+static inline bool led_state(void)
+{
+  return GPIO_PIN_RESET == HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_2);
+}
+
+/* Exported functions --------------------------------------------------------*/
 
 /**
   * @brief  The application entry point.
@@ -187,6 +202,7 @@ static void led_init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_SET);
@@ -197,26 +213,6 @@ static void led_init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-}
-
-static void led_on(void)
-{
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_RESET);
-}
-
-static void led_off(void)
-{
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_SET);
-}
-
-static void led_toggle(void)
-{
-  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_2);
-}
-
-static bool led_state(void)
-{
-  return GPIO_PIN_RESET == HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_2);
 }
 
 /**
