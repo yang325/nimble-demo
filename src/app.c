680e0807e4875e222c0643cd7c9da81a03cdf4e6
yangyufeng <yangyufeng@xiaomi.com>
2018-11-13 10:11:59 +0800

    enable overflow check

--- src/app.c
+++ src/app.c
@@ -296,4 +296,23 @@ void __assert_func(const char *file, int line, const char *func, const char *con
   }
 }
 
+/**
+ * @brief Loop forever if stack overflow is detected.
+ *
+ * If configCHECK_FOR_STACK_OVERFLOW is set to 1,
+ * this hook provides a location for applications to
+ * define a response to a stack overflow.
+ *
+ * Use this hook to help identify that a stack overflow
+ * has occurred.
+ *
+ */
+void vApplicationStackOverflowHook(TaskHandle_t xTask, char * pcTaskName)
+{
+  portDISABLE_INTERRUPTS();
+  console_printf("Application %s stack overflow\n", pcTaskName);
+  /* Generate breakpoint if debugger is connected */
+  __BKPT(0);
+}
+
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
