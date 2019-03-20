968458876da257b301feb2681147a2eafbe4cba6
yangyufeng <yangyufeng@xiaomi.com>
2019-03-20 21:44:25 +0800

    disable irq after output assert info

--- src/app.c
+++ src/app.c
@@ -225,10 +225,11 @@ static void ble_host_thread(void * arg)
   */
 void __assert_func(const char *file, int line, const char *func, const char *condition)
 {
-  taskDISABLE_INTERRUPTS();
   console_printf("Assert failed in %s, %s:%d (%s)", func, file, line, condition);
   /* Generate breakpoint if debugger is connected */
   __BKPT(0);
+  /* Disable IRQ */
+  __disable_irq();
   while(1)
   {
     led_toggle();
