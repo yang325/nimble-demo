21b2ef3fceb43776eefbc5d5413c3f2e481fb6a0
yangyufeng <yangyufeng@xiaomi.com>
2019-07-23 18:33:11 +0800

    update nimble repo and add mesh config server

--- src/demo.c
+++ src/demo.c
@@ -39,6 +39,26 @@ static const u32_t iv_index = 0x0000;
 static u8_t flags = 0;
 static u16_t dev_addr = 0x0001;
 
+static struct bt_mesh_cfg_srv cfg_srv = {
+	.relay = BT_MESH_RELAY_ENABLED,
+	.beacon = BT_MESH_BEACON_ENABLED,
+#if MYNEWT_VAL(BLE_MESH_FRIEND)
+	.frnd = BT_MESH_FRIEND_DISABLED,
+#else
+	.frnd = BT_MESH_FRIEND_NOT_SUPPORTED,
+#endif
+#if MYNEWT_VAL(BLE_MESH_GATT_PROXY)
+	.gatt_proxy = BT_MESH_GATT_PROXY_ENABLED,
+#else
+	.gatt_proxy = BT_MESH_GATT_PROXY_NOT_SUPPORTED,
+#endif
+	.default_ttl = 7,
+
+	/* 3 transmissions with 20ms interval */
+	.net_transmit = BT_MESH_TRANSMIT(2, 20),
+	.relay_retransmit = BT_MESH_TRANSMIT(2, 20),
+};
+
 static struct bt_mesh_cfg_cli cfg_cli = {
 };
 
@@ -47,6 +67,7 @@ static struct bt_mesh_health_cli health_cli = {
 };
 
 static struct bt_mesh_model root_models[] = {
+  BT_MESH_MODEL_CFG_SRV(&cfg_srv),
   BT_MESH_MODEL_CFG_CLI(&cfg_cli),
   BT_MESH_MODEL_HEALTH_CLI(&health_cli),
   BT_MESH_MODEL_GEN_ONOFF_CLI(),
